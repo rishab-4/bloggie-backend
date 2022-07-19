30fb8a06c9fa63d58250c63a223d8104b0602be8
Rishab Kumar Mishra <rishab228mishra@gmail.com>
2022-07-19 23:58:45 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+import cors from "cors";
+import * as dotenv from "dotenv";
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose
+  .connect(
+    process.env.MONGO_URL
+  )
+  .then(() => app.listen(process.env.PORT))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
